94b27c6283ccdb7ca5611cbd08264e664209592d
Alec Deitloff <alec.deitloff@gmail.com>
2017-09-02 08:57:10 -0700

    + Hooked up a web portal on top of the bot so that Heroku has something to bind to.

--- phil.js
+++ phil.js
@@ -3,10 +3,13 @@
 // Retrieve the modules
 const assert = require('assert');
 const discord = require('discord.io');
+const http = require('http');
+const express = require('express');
 
 // Make sure our environment is ready to operate
 require('dotenv').config({ silent: process.env.NODE_ENV === 'production' });
 assert.ok(process.env.DISCORD_BOT_TOKEN !== undefined);
+assert.ok(process.env.PORT !== undefined);
 
 // Connect to the bot
 const bot = new discord.Client( { token: process.env.DISCORD_BOT_TOKEN, autorun: true } );
@@ -23,3 +26,22 @@ bot.on('message', function(user, userId, channelId, message, event) {
         });
     }
 });
+
+// Set up the web portal
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/assets'));
+
+// Run web portal
+app.get('/', (request, response) => {
+	response.render('index');
+});
+
+app.listen(process.env.PORT, () => {
+	console.log('Web portal is running on port ' + process.env.PORT);
+});
+
+// Ping the server every 15 minutes so that the Heroku dynos won't fall asleepe
+setInterval(() => {
+	http.get(process.env.PUBLIC_APP_URL);
+}, 1000 * 60 * 15);
