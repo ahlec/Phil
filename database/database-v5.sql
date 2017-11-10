a3138f803b8e0bf1776d2989154e19c1c2edf857
Alec Deitloff <alec.deitloff@gmail.com>
2017-11-10 00:21:12 -0800

    + Timezone questionnaire has undergone MAJOR revisions. Now, instead of having the user provide their UTC offset and whether they observe daylight savings or not, the user will first provide the name of their country, and then choose from a list of timezones in their country if their country has multiple timezones. If their country only has one timezone, it'll automatically select it.
    NOTE: Country stage is finished, but Specification and Confirmation stages still need to be implemented.
    
    NOTE: data/ is generated via scripts/. However, it's static data and there isn't a reason why we wouldn't just submit it to the repository directly. The node modules added to package.json are only used within the new script, and their output is stored in a Phil-specific way inside of data/country-timezones.json

--- database/database-v5.sql
+++ database/database-v5.sql
@@ -1,10 +1,10 @@
 UPDATE info SET value = '5' WHERE key = 'database-version';
 
-CREATE TABLE timezone_questionnaire(
+CREATE TABLE timezones(
     username VARCHAR(100) NOT NULL,
     userid VARCHAR(40) NOT NULL,
     stage INTEGER NOT NULL,
     will_provide BIT(1),
-    utc_offset VARCHAR(10),
-    is_united_states BIT(1)
+    timezone_name VARCHAR(80),
+    country_name VARCHAR(40)
 );
--- phil/timezones.js
+++ phil/timezones.js
@@ -2,25 +2,17 @@
 
 const discord = require('../promises/discord');
 const chronoNode = require('chrono-node');
+const countryTimezones = require('../data/country-timezones.json');
 
 const QUESTIONNAIRE_STAGES = {
     None: 0,
     LetsBegin: 1,
-    UtcOffset: 2,
-    DaylightSavingsTime: 3,
-    Finished: 4,
-    Declined: 5
+    Country: 2,
+    Specification: 3,
+    Confirmation: 4,
+    Finished: 5,
+    Declined: 6
 };
-
-const QUESTIONNAIRE_MESSAGES = [
-    '<None>',
-    'Hey! You mentioned some times in your recent message on the server. Would you be willing to tell me what timezone you\'re in so that I can convert them to UTC in the future? Just say `yes` or `no`.',
-    'Alright! Let\'s get started! Can you tell me what your UTC offset is? It\'ll be something like `UTC-5:00` or `GMT+12:00` or `+9:30`. You can check https://whatismytimezone.com/ to find out quickly enough.',
-    'One more question: Are you in the United States? I\'m just asking to know if I should consider daylight savings time - that\'s all. Just a simple `yes` or `no` is good here.',
-    'All done! I\'ve recorded your timezone information! When you mention a date or time in the server again, I\'ll convert it for you! If you ever need to change it, just use `' + process.env.COMMAND_PREFIX + 'timezone` to do so!',
-    'Understood. I\'ve made a note that you don\'t want to provide this information at this time. I won\'t bother you again. If you ever change your mind, feel free to say `' + process.env.COMMAND_PREFIX + 'timezone` to start up again.'
-];
-
 
 function processDatabaseTimezoneResults(results) {
     if (results.rowCount === 0) {
@@ -60,26 +52,69 @@ function canStartQuestionnaire(db, userId, manuallyStartedQuestionnaire) {
         return true;
     }
 
-    return db.query('SELECT will_provide FROM timezone_questionnaire WHERE userid = $1', [userId])
+    return db.query('SELECT will_provide FROM timezones WHERE userid = $1', [userId])
         .then(interpretCanStartDbResult);
 }
 
-function clearPreviousQuestionnaire(db, userId) {
-    return db.query('DELETE FROM timezone_questionnaire WHERE userid = $1', [userId]);
+function clearPreviousData(db, userId) {
+    return db.query('DELETE FROM timezones WHERE userid = $1', [userId]);
 }
 
-function insertQuestionnaireDbRow(bot, db, userId, initialStage) {
+function insertNewTimezoneRow(bot, db, userId, initialStage) {
     const username = bot.users[userId].username;
-    return db.query('INSERT INTO timezone_questionnaire(username, userid, stage) VALUES($1, $2, $3)', [username, userId, initialStage]);
+    return db.query('INSERT INTO timezones(username, userid, stage) VALUES($1, $2, $3)', [username, userId, initialStage]);
 }
 
-function sendStageMessage(bot, userId, stage) {
-    const message = QUESTIONNAIRE_MESSAGES[stage];
-    if (!message || message.length === 0) {
-        return Promise.reject('There was no message defined for questionnaire stage `' + stage + '`.');
+function buildSpecificationListFromCountryTimezones(timezoneData) {
+    var message = 'Okay! Your country has a couple of timezones. Please tell me the **number** next to the ';
+    if (timezoneData.isCities) {
+        message += 'city closest to you that\'s in your timezone';
+    } else {
+        message += 'timezone that you\'re in';
     }
-    
-    return discord.sendMessage(bot, userId, message);
+    message += ':\n\n';
+
+    for (let index = 0; index < timezoneData.timezones.length; ++index) {
+        message += '`' + (index + 1) + '`: ' + timezoneData.timezones[index].displayName + '\n';
+    }
+
+    message += '\nAgain, just tell me the **number**. It\'s easier that way than making you type out the whole name, y\'know?';
+    return message;
+}
+
+const GetQuestionnaireStageMessageFuncs = {};
+GetQuestionnaireStageMessageFuncs[QUESTIONNAIRE_STAGES.None] = function(db, userId) {
+    return Promise.resolve('<None>');
+};
+GetQuestionnaireStageMessageFuncs[QUESTIONNAIRE_STAGES.LetsBegin] = function(db, userId) {
+    return Promise.resolve('Hey! You mentioned some times in your recent message on the server. Would you be willing to tell me what timezone you\'re in so that I can convert them to UTC in the future? Just say `yes` or `no`.');
+};
+GetQuestionnaireStageMessageFuncs[QUESTIONNAIRE_STAGES.Country] = function(db, userId) {
+    return Promise.resolve('Alright! Let\'s get started! Can you start by telling me the name of the country you\'re in? I\'ll never display this information publicly in the chat.');
+};
+GetQuestionnaireStageMessageFuncs[QUESTIONNAIRE_STAGES.Specification] = function(db, userId) {
+    return db.query('SELECT country_name FROM timezones WHERE userid = $1', [userId])
+        .then(results => countryTimezones[results.rows[0].country_name])
+        .then(buildSpecificationListFromCountryTimezones);
+};
+GetQuestionnaireStageMessageFuncs[QUESTIONNAIRE_STAGES.Confirmation] = function(db, userId) {
+    return null;
+};
+GetQuestionnaireStageMessageFuncs[QUESTIONNAIRE_STAGES.Finished] = function(db, userId) {
+    return Promise.resolve('All done! I\'ve recorded your timezone information! When you mention a date or time in the server again, I\'ll convert it for you! If you ever need to change it, just use `' + process.env.COMMAND_PREFIX + 'timezone` to do so!');
+};
+GetQuestionnaireStageMessageFuncs[QUESTIONNAIRE_STAGES.Declined] = function(db, userId) {
+    return Promise.resolve('Understood. I\'ve made a note that you don\'t want to provide this information at this time. I won\'t bother you again. If you ever change your mind, feel free to say `' + process.env.COMMAND_PREFIX + 'timezone` to start up again.');
+};
+
+function sendStageMessage(bot, db, userId, stage) {
+    const messageFunc = GetQuestionnaireStageMessageFuncs[stage];
+    if (!messageFunc) {
+        return Promise.reject('There was no message function defined for questionnaire stage `' + stage + '`.');
+    }
+
+    return messageFunc(db, userId)
+        .then(message => discord.sendMessage(bot, userId, message));
 }
 
 function branchStartQuestionnaire(bot, db, userId, manuallyStartedQuestionnaire, canStart) {
@@ -87,12 +122,12 @@ function branchStartQuestionnaire(bot, db, userId, manuallyStartedQuestionnaire,
         return false;
     }
 
-    const initialStage = (manuallyStartedQuestionnaire ? QUESTIONNAIRE_STAGES.UtcOffset : QUESTIONNAIRE_STAGES.LetsBegin);
+    const initialStage = (manuallyStartedQuestionnaire ? QUESTIONNAIRE_STAGES.Country : QUESTIONNAIRE_STAGES.LetsBegin);
 
     return Promise.resolve()
-        .then(() => clearPreviousQuestionnaire(db, userId))
-        .then(() => insertQuestionnaireDbRow(bot, db, userId, initialStage))
-        .then(() => sendStageMessage(bot, userId, initialStage))
+        .then(() => clearPreviousData(db, userId))
+        .then(() => insertNewTimezoneRow(bot, db, userId, initialStage))
+        .then(() => sendStageMessage(bot, db, userId, initialStage))
         .then(() => true);
 }
 
@@ -112,20 +147,20 @@ function ensureRowWasModified(results) {
 }
 
 function setStage(bot, db, userId, stage) {
-    return db.query('UPDATE timezone_questionnaire SET stage = $1 WHERE userid = $2', [stage, userId])
+    return db.query('UPDATE timezones SET stage = $1 WHERE userid = $2', [stage, userId])
         .then(ensureRowWasModified)
-        .then(() => sendStageMessage(bot, userId, stage));
+        .then(() => sendStageMessage(bot, db, userId, stage));
 }
 
 function processLetsBeginStage(bot, db, message) {
     const content = message.content.toLowerCase().trim();
 
     if (content === 'yes') {
-        return setStage(bot, db, message.userId, QUESTIONNAIRE_STAGES.UtcOffset);
+        return setStage(bot, db, message.userId, QUESTIONNAIRE_STAGES.Country);
     }
-    
+
     if (content === 'no') {
-        return db.query('UPDATE timezone_questionnaire SET will_provide = E\'0\' WHERE userid = $1', [message.userId])
+        return db.query('UPDATE timezones SET will_provide = E\'0\' WHERE userid = $1', [message.userId])
             .then(ensureRowWasModified)
             .then(setStage(bot, db, message.userId, QUESTIONNAIRE_STAGES.Declined));
     }
@@ -133,38 +168,24 @@ function processLetsBeginStage(bot, db, message) {
     return discord.sendMessage(bot, message.channelId, 'I didn\'t understand that, sorry. Can you please tell me `yes` or `no` for if you\'d like to fill out the timezone questionnaire?');
 }
 
-function parseUtcOffset(text) {
-    if (!text || text.length === 0) {
-        return null;
+function processCountryStage(bot, db, message) {
+    const input = message.content.trim().toLowerCase();
+    const timezoneData = countryTimezones[input];
+
+    if (!timezoneData) {
+        return discord.sendMessage(bot, message.channelId, 'I\'m not sure what country that was. I can understand a country by a couple of names, but the easiest is the standard English name of the country.');
     }
 
-    text = text.trim().toLowerCase();
-    const utcRegex = /^\s*(utc|gmt)?\s*((\+|\-)\0?[0-9](:(0|3)0)?)\s*$/i; // Group #2 will be the entire offset, +4:00 or -12:00 or -7
-    const matches = utcRegex.exec(text);
-
-    if (!matches) {
-        return null;
+    if (timezoneData.timezones.length === 1) {
+        const timezoneName = timezoneData.timezones[0].name;
+        return db.query('UPDATE timezones SET timezone_name = $1 WHERE userid = $2', [timezoneName, message.userId])
+            .then(ensureRowWasModified)
+            .then(setStage(bot, db, message.userId, QUESTIONNAIRE_STAGES.Confirmation));
     }
 
-    var utcOffset = matches[2];
-
-    if (utcOffset.indexOf(':') < 0) {
-        utcOffset += ':00';
-    }
-
-    return utcOffset;
-}
-
-function processUtcOffsetStage(bot, db, message) {
-    const utcOffset = parseUtcOffset(message.content);
-
-    if (!utcOffset) {
-        return discord.sendMessage(bot, message.channelId, 'I\'m not sure I understood that. Could you tell me your UTC offset, like `UTC-05:00` or `+12:30`?');
-    }
-
-    return db.query('UPDATE timezone_questionnaire SET utc_offset = $1 WHERE userid = $2', [utcOffset, message.userId])
+    return db.query('UPDATE timezones SET country_name = $1 WHERE userid = $2', [input, message.userId])
         .then(ensureRowWasModified)
-        .then(setStage(bot, db, message.userId, QUESTIONNAIRE_STAGES.DaylightSavingsTime));
+        .then(setStage(bot, db, message.userId, QUESTIONNAIRE_STAGES.Specification));
 }
 
 function processDaylightSavingStage(bot, db, message) {
@@ -196,9 +217,9 @@ function processQuestionnaireStage(bot, db, message, results) {
         {
             return processLetsBeginStage(bot, db, message);
         }
-        case QUESTIONNAIRE_STAGES.UtcOffset:
+        case QUESTIONNAIRE_STAGES.Country:
         {
-            return processUtcOffsetStage(bot, db, message);
+            return processCountryStage(bot, db, message);
         }
         case QUESTIONNAIRE_STAGES.DaylightSavingsTime:
         {
@@ -217,7 +238,7 @@ module.exports = {
     },
 
     hasDeclinedProvidingTimezone: function(db, userId) { // resolves [true/false] for if the user has declined to provide their timezone
-        return db.query('SELECT will_provide FROM timezone_questionnaire WHERE userid = $1 LIMIT 1', [userId])
+        return db.query('SELECT will_provide FROM timezones WHERE userid = $1 LIMIT 1', [userId])
             .then(processDeclinedProvidingResults);
     },
 
@@ -228,12 +249,12 @@ module.exports = {
     },
 
     isCurrentlyDoingQuestionnaire: function(db, userId) { // resolves [true/false] for if currently doing questionnaire
-        return db.query('SELECT stage FROM timezone_questionnaire WHERE userid = $1 LIMIT 1', [userId])
+        return db.query('SELECT stage FROM timezones WHERE userid = $1 LIMIT 1', [userId])
             .then(processIsCurrentlyDoingQuestionnaireResults);
     },
 
     processQuestionnaireMessage: function(bot, db, message) {
-        return db.query('SELECT stage FROM timezone_questionnaire WHERE userid = $1 LIMIT 1', [message.userId])
+        return db.query('SELECT stage FROM timezones WHERE userid = $1 LIMIT 1', [message.userId])
             .then(results => processQuestionnaireStage(bot, db, message, results));
     }
 };
