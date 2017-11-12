f9a74e33273953cba9b54c8cbfd90b3f15b0cfb6
Alec Deitloff <alec.deitloff@gmail.com>
2017-11-12 01:43:25 -0800

    + Rewrote the booty day, birthday, and daily prompt chronos so that their promise code matched the style that we're using everywhere else with Phil. I believe that chronos are the only piece of code that hasn't been touched up since they were originally written, and they're very difficult to maintain.

--- chronos/booty_day.js
+++ chronos/booty_day.js
@@ -1,67 +1,65 @@
-module.exports = (function() {
-    'use strict';
+'use strict';
 
-    const discord = require('../promises/discord');
-    const DATABASE_INFO_KEY = 'booty-day-last-reminded';
+const discord = require('../promises/discord');
+const DATABASE_INFO_KEY = 'booty-day-last-reminded';
 
-    function interpretLastRemindedResults(results, now) {
-        if (results.rowCount !== 1) {
-            return Promise.reject('There is no database record for `' + DATABASE_INFO_KEY + '`');
-        }
-
-        const dateLastReminded = new Date(results.rows[0].value);
-        if (isNaN(dateLastReminded)) {
-            return Promise.reject('The value for the last booty day reminder was not a number in the database.');
-        }
-
-        if (now.getUTCFullYear() != dateLastReminded.getUTCFullYear()) {
-            return false;
-        }
-
-        return (now.getUTCMonth() == dateLastReminded.getUTCMonth());
+function interpretLastRemindedResults(results, now) {
+    if (results.rowCount !== 1) {
+        return Promise.reject('There is no database record for `' + DATABASE_INFO_KEY + '`');
     }
 
-    function hasAlreadyRemindedBootyDayThisMonth(db, now) {
-        return db.query('SELECT value FROM info WHERE key = $1', [DATABASE_INFO_KEY])
-            .then(results => interpretLastRemindedResults(results, now));
+    const dateLastReminded = new Date(results.rows[0].value);
+    if (isNaN(dateLastReminded)) {
+        return Promise.reject('The value for the last booty day reminder was not a number in the database.');
     }
 
-    function canProcessFromHasRemindedThisMonth(hasReminded) {
-        return {
-            ready: !hasReminded
-        };
+    if (now.getUTCFullYear() != dateLastReminded.getUTCFullYear()) {
+        return false;
     }
 
-    function ensureDatabaseUpdated(results) {
-        if (results.rowCount !== 1) {
-            return Promise.reject(
-                'No database rows were updated when attempting to record that booty day was reported today.');
-        }
-    }
+    return (now.getUTCMonth() == dateLastReminded.getUTCMonth());
+}
 
-    function markRemindedInDatabase(db, now) {
-        return db.query('UPDATE info SET value = $1 WHERE key = $2', [now, DATABASE_INFO_KEY])
-            .then(ensureDatabaseUpdated);
-    }
+function hasAlreadyRemindedBootyDayThisMonth(db, now) {
+    return db.query('SELECT value FROM info WHERE key = $1', [DATABASE_INFO_KEY])
+        .then(results => interpretLastRemindedResults(results, now));
+}
 
+function canProcessFromHasRemindedThisMonth(hasReminded) {
     return {
-        name: 'booty day',
-        hourUtc: 7, // 2am EST, 11pm PST
-
-        canProcess: function(chronosManager, now, bot, db) {
-            if (now.getUTCDate() !== 3) {
-                return Promise.resolve({
-                    ready: false
-                });
-            }
-
-            return hasAlreadyRemindedBootyDayThisMonth(db, now)
-                .then(canProcessFromHasRemindedThisMonth);
-        },
-
-        process: function(chronosManager, now, bot, db) {
-            return markRemindedInDatabase(db, now)
-                .then(() => discord.sendMessage(bot, process.env.NEWS_CHANNEL_ID, process.env.CUSTOM_EMOJI_PEEK + ' It\'s booty day! Post your Hijack booties!'));
-        }
+        ready: !hasReminded
     };
-})();
+}
+
+function ensureDatabaseUpdated(results) {
+    if (results.rowCount !== 1) {
+        return Promise.reject(
+            'No database rows were updated when attempting to record that booty day was reported today.');
+    }
+}
+
+function markRemindedInDatabase(db, now) {
+    return db.query('UPDATE info SET value = $1 WHERE key = $2', [now, DATABASE_INFO_KEY])
+        .then(ensureDatabaseUpdated);
+}
+
+module.exports = {
+    name: 'booty day',
+    hourUtc: 7, // 2am EST, 11pm PST
+
+    canProcess: function(chronosManager, now, bot, db) {
+        if (now.getUTCDate() !== 3) {
+            return Promise.resolve({
+                ready: false
+            });
+        }
+
+        return hasAlreadyRemindedBootyDayThisMonth(db, now)
+            .then(canProcessFromHasRemindedThisMonth);
+    },
+
+    process: function(chronosManager, now, bot, db) {
+        return markRemindedInDatabase(db, now)
+            .then(() => discord.sendMessage(bot, process.env.NEWS_CHANNEL_ID, process.env.CUSTOM_EMOJI_PEEK + ' It\'s booty day! Post your Hijack booties!'));
+    }
+};
--- chronos/new_daily_prompt.js
+++ chronos/new_daily_prompt.js
@@ -1,121 +1,99 @@
-module.exports = (function() {
-    'use strict';
+'use strict';
 
-    const features = require('../phil/features');
-    const botUtils = require('../phil/utils');
-    const prompts = require('../phil/prompts');
-    const discord = require('../promises/discord');
+const features = require('../phil/features');
+const botUtils = require('../phil/utils');
+const prompts = require('../phil/prompts');
+const discord = require('../promises/discord');
 
-    function _processQueueLength(bot, queueLength) {
-        if (queueLength >= process.env.PROMPT_QUEUE_EMPTY_ALERT_THRESHOLD) {
-            return;
-        }
-
-        var are = (queueLength == 1 ? 'is' : 'are');
-        var promptNoun = (queueLength == 1 ? 'prompt' : 'prompts');
-
-        var message = ':warning: The prompt queue is growing short. There ' + are + ' **' + (queueLength > 0 ? queueLength : 'no') + '** more ' + promptNoun + ' in the queue.';
-        return discord.sendMessage(bot, process.env.BOT_CONTROL_CHANNEL_ID, message);
+function branchCanProcessTodaysPrompt(chronosManager, now, bot, db, prompt) {
+    if (!prompt) {
+        return Promise.resolve({
+            ready: false
+        });
     }
 
-    function _handleRemainingQueue(db, bot) {
-        if (process.env.PROMPT_QUEUE_EMPTY_ALERT_THRESHOLD <= 0) {
-            return;
-        }
-
-        return prompts.getPromptQueueLength(db)
-            .then(queueLength => _processQueueLength(bot, queueLength));
+    const minutesSinceLastHijackMessage = chronosManager.getMinutesSinceLastMessageInChannel(process.env.HIJACK_CHANNEL_ID, now);
+    if (minutesSinceLastHijackMessage < chronosManager.MinimumSilenceRequiredBeforePostingInChannel) {
+        return Promise.resolve({
+            ready: false,
+            retryIn: (chronosManager.MinimumSilenceRequiredBeforePostingInChannel - minutesSinceLastHijackMessage + 1)
+        });
     }
 
-    function _handleHasBeenPostedResults(results, bot, promptNumber, promptText) {
-        if (results.rowCount === 0) {
-            return Promise.reject('We found a prompt in the queue, but we couldn\'t update it to mark it as being posted.');
-        }
+    return Promise.resolve({
+        ready: true
+    });
+}
 
-        return prompts.sendPromptToChannel(bot, process.env.HIJACK_CHANNEL_ID, promptNumber, promptText );
+function branchCanProcessFeatureEnabled(chronosManager, now, bot, db, isEnabled) {
+    if (!isEnabled) {
+        return Promise.resolve({
+            ready: false
+        });
     }
 
-    function selectNextUnpublishedPromptAndContinue(chronosManager, now, bot, db, promptNumber) {
-        return prompts.getPromptQueue(db, 1)
-            .then(queue => {
-                if (queue.length === 0) {
-                    return Promise.resolve(false);
-                }
+    return prompts.getTodaysPrompt(db)
+        .then(prompt => branchCanProcessTodaysPrompt(chronosManager, now, bot, db, prompt));
+}
 
-                const promptId = queue[0].promptId;
-                const promptText = queue[0].promptText;
-                return db.query('UPDATE hijack_prompts SET has_been_posted = E\'1\', prompt_number = $1, prompt_date = $2 WHERE prompt_id = $3', [promptNumber, now, promptId])
-                    .then(results => _handleHasBeenPostedResults(results, bot, promptNumber, promptText))
-                    .then(() => _handleRemainingQueue(db, bot))
-                    .then(() => true);
-            });
+function handleHasBeenPostedResults(results, bot, promptNumber, promptText) {
+    if (results.rowCount === 0) {
+        return Promise.reject('We found a prompt in the queue, but we couldn\'t update it to mark it as being posted.');
     }
 
-    function getNextPromptNumberAndContinue(chronosManager, now, bot, db) {
+    return prompts.sendPromptToChannel(bot, process.env.HIJACK_CHANNEL_ID, promptNumber, promptText );
+}
+
+function postNewPrompt(chronosManager, now, bot, db, promptNumber) {
+    return prompts.getPromptQueue(db, 1)
+        .then(queue => {
+            if (queue.length === 0) {
+                return Promise.resolve(false);
+            }
+
+            const promptId = queue[0].promptId;
+            const promptText = queue[0].promptText;
+            return db.query('UPDATE hijack_prompts SET has_been_posted = E\'1\', prompt_number = $1, prompt_date = $2 WHERE prompt_id = $3', [promptNumber, now, promptId])
+                .then(results => handleHasBeenPostedResults(results, bot, promptNumber, promptText))
+                .then(() => true);
+        });
+}
+
+function alertQueueDwindling(bot, queueLength) {
+    if (queueLength >= process.env.PROMPT_QUEUE_EMPTY_ALERT_THRESHOLD) {
+        return;
+    }
+
+    var are = (queueLength == 1 ? 'is' : 'are');
+    var promptNoun = (queueLength == 1 ? 'prompt' : 'prompts');
+
+    var message = ':warning: The prompt queue is growing short. There ' + are + ' **' + (queueLength > 0 ? queueLength : 'no') + '** more ' + promptNoun + ' in the queue.';
+    return discord.sendMessage(bot, process.env.BOT_CONTROL_CHANNEL_ID, message);
+}
+
+function handleRemainingQueue(db, bot) {
+    if (process.env.PROMPT_QUEUE_EMPTY_ALERT_THRESHOLD <= 0) {
+        return;
+    }
+
+    return prompts.getPromptQueueLength(db)
+        .then(queueLength => alertQueueDwindling(bot, queueLength));
+}
+
+module.exports = {
+    name: 'new daily prompt',
+    hourUtc: 12, // 8am EST, 5am PST
+
+    canProcess: function(chronosManager, now, bot, db) {
+        return features.getIsFeatureEnabled(features.Features.DailyPrompts, db)
+            .then(isEnabled => branchCanProcessFeatureEnabled(chronosManager, now, bot, db, isEnabled));
+    },
+
+    process: function(chronosManager, now, bot, db) {
         return db.query('SELECT prompt_number FROM hijack_prompts WHERE has_been_posted = E\'1\' ORDER BY prompt_number DESC LIMIT 1')
-            .then(promptNumberResults => {
-                var promptNumber = 1;
-                if (promptNumberResults.rowCount > 0) {
-                    promptNumber = promptNumberResults.rows[0].prompt_number + 1;
-                }
-
-                return selectNextUnpublishedPromptAndContinue(chronosManager, now, bot, db, promptNumber);
-            });
+            .then(results => (results.rowCount > 0 ? results.rows[0].prompt_number + 1 : 1))
+            .then(promptNumber => postNewPrompt(chronosManager, now, bot, db, promptNumber))
+            .then(() => handleRemainingQueue(db, bot))
+            .then(() => true);
     }
-
-    return {
-        name: 'new daily prompt',
-        hourUtc: 12, // 8am EST, 5am PST
-        canProcess: function(chronosManager, now, bot, db) {
-            return new Promise((resolve, reject) => {
-                db.query('SELECT count(*) FROM hijack_prompts WHERE prompt_date = $1', [now])
-                    .then(results => {
-                        if (results.rows[0].count > 0) {
-                            console.log('1');
-                            resolve({
-                                ready: false
-                            }); // There's already a prompt for today
-                            return;
-                        }
-
-                        const minutesSinceLastHijackMessage = chronosManager.getMinutesSinceLastMessageInChannel(process.env.HIJACK_CHANNEL_ID, now);
-                        if (minutesSinceLastHijackMessage < chronosManager.MinimumSilenceRequiredBeforePostingInChannel) {
-                            console.log('2');
-                            resolve({
-                                ready: false,
-                                retryIn: (chronosManager.MinimumSilenceRequiredBeforePostingInChannel - minutesSinceLastHijackMessage + 1)
-                            });
-                        } else {
-                            console.log('3');
-                            resolve({
-                                ready: true
-                            });
-                        }
-                    })
-                    .catch(err => {
-                        reject(err);
-                    })
-            });
-        },
-        process: function(chronosManager, now, bot, db) {
-            return new Promise((resolve, reject) => {
-                features.getIsFeatureEnabled(features.Features.DailyPrompts, db)
-                    .then(arePromptsEnabled => {
-                        if (!arePromptsEnabled) {
-                            console.log('prompts are disabled, so we won\'t process a new prompt today.');
-                            resolve(false);
-                            return;
-                        }
-
-                        getNextPromptNumberAndContinue(chronosManager, now, bot, db)
-                            .then(result => {
-                                resolve(result);
-                            });
-                    })
-                    .catch(err => {
-                        reject(err);
-                    });
-            });
-        }
-    };
-})();
+};
