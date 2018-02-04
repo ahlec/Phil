dca6181c1f046d7fe3e8c7d3cd1049376ea7e1d7
Alec Deitloff <alec.deitloff@gmail.com>
2018-02-03 21:25:46 -0800

    Added p!news, an admin command that makes Phil echo text provided in the news channel so that it appears as though the bot is speaking, rather than the admin. Useful for when you want something to be presented by the bot rather than by the admin.

---
+++ commands/news.js
@@ -0,0 +1,32 @@
+module.exports = (function() {
+    'use strict';
+
+    const helpGroups = require('../phil/help-groups');
+    const discord = require('../promises/discord');
+
+    function getEchoedStatementFromCommandArgs(commandArgs) {
+        var echoedMessage = commandArgs.join(' ').trim();
+        echoedMessage = echoedMessage.replace(/`/g, '');
+
+        if (echoedMessage.length === 0) {
+            return Promise.reject('You must provide a message to this function that you would like Phil to repeat in #news. For instance, `' + process.env.COMMAND_PREFIX + 'news A New Guardian has been Chosen!`');
+        }
+
+        return echoedMessage;
+    }
+
+    return {
+        aliases: [],
+
+        helpGroup: helpGroups.Groups.Admin,
+        helpDescription: 'Has Phil echo the message provided in the news channel.',
+        versionAdded: 11,
+
+        publicRequiresAdmin: false,
+        processPublicMessage: function(bot, message, commandArgs, db) {
+            return Promise.resolve()
+                .then(() => getEchoedStatementFromCommandArgs(commandArgs))
+                .then(echoedMessage => discord.sendMessage(bot, process.env.NEWS_CHANNEL_ID, echoedMessage));
+        }
+    };
+})();
