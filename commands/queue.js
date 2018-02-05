a47db26e9280016b9d5ac12fb0562c1230e9fae9
Alec Deitloff <alec.deitloff@gmail.com>
2018-02-04 16:51:31 -0800

    Moved the code to retrieve a single bucket from the commandArgs to buckets.js, so that we could reuse it between p!queue and p!unconfirmed (and potentially other commands in the future)

--- commands/queue.js
+++ commands/queue.js
@@ -6,72 +6,7 @@ module.exports = (function() {
     const buckets = require('../phil/buckets');
     const helpGroups = require('../phil/help-groups');
     const discord = require('../promises/discord');
-    const botUtils = require('../phil/utils');
     const MAX_QUEUE_DISPLAY_LENGTH = 10;
-
-    function _createMultipleUnspecifiedBucketsError(serverBuckets) {
-        if (serverBuckets.length === 0) {
-            return Promise.reject('There are no prompt buckets configured on this server.');
-        }
-
-        var message = 'This command must be provided the valid reference handle of one of the buckets configured on this server:\n\n';
-
-        for (let bucket of serverBuckets) {
-            message += '`' + bucket.handle + '` - ' + bucket.displayName + ' (';
-
-            if (bucket.isValid) {
-                message += 'posts to <#' + bucket.channelId + '>';
-            } else {
-                message += 'configuration invalid'
-            }
-
-            message += ')\n';
-        }
-
-        const randomBucket = botUtils.getRandomArrayEntry(serverBuckets);
-        message += '\nPlease try the command once more, specifying which bucket, like `' + process.env.COMMAND_PREFIX + 'queue ' + randomBucket.handle + '`.';
-        return Promise.reject(message);
-    }
-
-    function _getOnlyBucketOnServer(serverBuckets) {
-        if (serverBuckets.length === 1 && serverBuckets[0].isValid) {
-            return serverBuckets[0];
-        }
-
-        return _createMultipleUnspecifiedBucketsError(serverBuckets);
-    }
-
-    function getFirstParameterFromCommandArgs(commandArgs) {
-        const input = commandArgs.join(' ').trim();
-        if (input.length === 0) {
-            return null;
-        }
-
-        const indexFirstSpace = input.indexOf(' ');
-        if (indexFirstSpace < 0) {
-            return input;
-        }
-
-        return input.substr(0, indexFirstSpace);
-    }
-
-    function retrieveBucket(bot, db, commandArgs, server) {
-        const firstParameter = getFirstParameterFromCommandArgs(commandArgs);
-        if (!firstParameter || firstParameter.length === 0) {
-            return buckets.getAllForServer(bot, db, server)
-                .then(_getOnlyBucketOnServer);
-        }
-
-        return buckets.getFromReferenceHandle(bot, db, firstParameter)
-            .then(bucket => {
-                if (bucket === null || !bucket.isValid) {
-                    return buckets.getAllForServer(bot, db, server)
-                        .then(_createMultipleUnspecifiedBucketsError);
-                }
-
-                return bucket;
-            })
-    }
 
     function _makeMessageOutOfQueue(queue) {
         if (queue.length === 0) {
@@ -105,7 +40,7 @@ module.exports = (function() {
         publicRequiresAdmin: true,
         processPublicMessage: function(bot, message, commandArgs, db) {
             return features.ensureFeatureIsEnabled(features.Features.DailyPrompts, db)
-                .then(() => retrieveBucket(bot, db, commandArgs, message.server))
+                .then(() => buckets.retrieveFromCommandArgs(bot, db, commandArgs, message.server, 'queue'))
                 .then(bucket => prompts.getPromptQueue(db, bot, bucket, MAX_QUEUE_DISPLAY_LENGTH))
                 .then(_makeMessageOutOfQueue)
                 .then(response => discord.sendMessage(bot, message.channelId, response));
--- phil/buckets.js
+++ phil/buckets.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const assert = require('assert');
+const botUtils = require('../phil/utils');
 
 function parseBucketDbResult(dbRow, bot) {
     const server = bot.servers[dbRow.server_id];
@@ -15,41 +16,109 @@ function parseBucketDbResult(dbRow, bot) {
     };
 }
 
+function _getFirstParameterFromCommandArgs(commandArgs) {
+    const input = commandArgs.join(' ').trim();
+    if (input.length === 0) {
+        return null;
+    }
+
+    const indexFirstSpace = input.indexOf(' ');
+    if (indexFirstSpace < 0) {
+        return input;
+    }
+
+    return input.substr(0, indexFirstSpace);
+}
+
+function _createMultipleUnspecifiedBucketsError(serverBuckets, commandName) {
+    if (serverBuckets.length === 0) {
+        return Promise.reject('There are no prompt buckets configured on this server.');
+    }
+
+    var message = 'This command must be provided the valid reference handle of one of the buckets configured on this server:\n\n';
+
+    for (let bucket of serverBuckets) {
+        message += '`' + bucket.handle + '` - ' + bucket.displayName + ' (';
+
+        if (bucket.isValid) {
+            message += 'posts to <#' + bucket.channelId + '>';
+        } else {
+            message += 'configuration invalid'
+        }
+
+        message += ')\n';
+    }
+
+    const randomBucket = botUtils.getRandomArrayEntry(serverBuckets);
+    message += '\nPlease try the command once more, specifying which bucket, like `' + process.env.COMMAND_PREFIX + commandName + ' ' + randomBucket.handle + '`.';
+    return Promise.reject(message);
+}
+
+function _getOnlyBucketOnServer(serverBuckets, commandName) {
+    if (serverBuckets.length === 1 && serverBuckets[0].isValid) {
+        return serverBuckets[0];
+    }
+
+    return _createMultipleUnspecifiedBucketsError(serverBuckets, commandName);
+}
+
+function getFromChannelId(bot, db, channelId) {
+    return db.query('SELECT bucket_id, server_id, channel_id, reference_handle, display_name FROM prompt_buckets WHERE channel_id = $1', [channelId])
+        .then(results => {
+            if (results.rowCount === 0) {
+                return null;
+            }
+
+            assert(results.rowCount === 1);
+            return parseBucketDbResult(results.rows[0], bot);
+        });
+}
+
+function getFromReferenceHandle(bot, db, referenceHandle) {
+    return db.query('SELECT bucket_id, server_id, channel_id, reference_handle, display_name FROM prompt_buckets WHERE reference_handle = $1', [referenceHandle])
+        .then(results => {
+            if (results.rowCount === 0) {
+                return null;
+            }
+
+            assert(results.rowCount === 1);
+            return parseBucketDbResult(results.rows[0], bot);
+        });
+}
+
+function getAllForServer(bot, db, server) {
+    return db.query('SELECT bucket_id, server_id, channel_id, reference_handle, display_name FROM prompt_buckets WHERE server_id = $1', [server.id])
+        .then(results => {
+            const buckets = [];
+
+            for (let index = 0; index < results.rowCount; ++index) {
+                buckets.push(parseBucketDbResult(results.rows[index], bot));
+            }
+
+            return buckets;
+        });
+}
+
 module.exports = {
-    getFromChannelId: function(bot, db, channelId) {
-        return db.query('SELECT bucket_id, server_id, channel_id, reference_handle, display_name FROM prompt_buckets WHERE channel_id = $1', [channelId])
-            .then(results => {
-                if (results.rowCount === 0) {
-                    return null;
+    getFromChannelId: getFromChannelId,
+    getFromReferenceHandle: getFromReferenceHandle,
+    getAllForServer: getAllForServer,
+
+    retrieveFromCommandArgs: function(bot, db, commandArgs, server, commandName) {
+        const firstParameter = _getFirstParameterFromCommandArgs(commandArgs);
+        if (!firstParameter || firstParameter.length === 0) {
+            return getAllForServer(bot, db, server)
+                .then(serverBuckets => _getOnlyBucketOnServer(serverBuckets, commandName));
+        }
+
+        return getFromReferenceHandle(bot, db, firstParameter)
+            .then(bucket => {
+                if (bucket === null || !bucket.isValid) {
+                    return getAllForServer(bot, db, server)
+                        .then(serverBuckets => _createMultipleUnspecifiedBucketsError(serverBuckets, commandName));
                 }
 
-                assert(results.rowCount === 1);
-                return parseBucketDbResult(results.rows[0], bot);
+                return bucket;
             });
-    },
-
-    getFromReferenceHandle: function(bot, db, referenceHandle) {
-        return db.query('SELECT bucket_id, server_id, channel_id, reference_handle, display_name FROM prompt_buckets WHERE reference_handle = $1', [referenceHandle])
-            .then(results => {
-                if (results.rowCount === 0) {
-                    return null;
-                }
-
-                assert(results.rowCount === 1);
-                return parseBucketDbResult(results.rows[0], bot);
-            });
-    },
-
-    getAllForServer: function(bot, db, server) {
-        return db.query('SELECT bucket_id, server_id, channel_id, reference_handle, display_name FROM prompt_buckets WHERE server_id = $1', [server.id])
-            .then(results => {
-                const buckets = [];
-
-                for (let index = 0; index < results.rowCount; ++index) {
-                    buckets.push(parseBucketDbResult(results.rows[index], bot));
-                }
-
-                return buckets;
-            });
-    },
+    }
 };
