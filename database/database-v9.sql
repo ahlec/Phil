d4a68c1db9df4a4065becf55b52a9a9466737e49
Alec Deitloff <alec.deitloff@gmail.com>
2019-01-10 09:00:31 -0800

    start work on reusable prompt transition

---
+++ database/database-v9.sql
@@ -0,0 +1,58 @@
+CREATE TABLE submission(
+  submission_id SERIAL NOT NULL UNIQUE,
+  bucket_id INTEGER NOT NULL REFERENCES prompt_buckets(bucket_id),
+  suggesting_userid VARCHAR(40) NOT NULL,
+  date_suggested TIMESTAMP NOT NULL,
+  approved_by_admin BIT(1) NOT NULL DEFAULT E'0',
+  submitted_anonymously BIT(1) NOT NULL DEFAULT E'0',
+  submission_text TEXT NOT NULL
+);
+
+CREATE TABLE prompt_v2(
+  prompt_id SERIAL NOT NULL UNIQUE,
+  submission_id INTEGER NOT NULL REFERENCES submission(submission_id),
+  prompt_number INTEGER NOT NULL,
+  prompt_date DATE NULL DEFAULT NULL,
+  has_been_posted BIT(1) NOT NULL DEFAULT E'0'
+);
+
+INSERT INTO
+  submission (
+    bucket_id,
+    suggesting_userid,
+    date_suggested,
+    approved_by_admin,
+    submitted_anonymously,
+    submission_text
+  )
+SELECT
+  bucket_id,
+  suggesting_userid,
+  date_suggested,
+  approved_by_admin,
+  submitted_anonymously,
+  prompt_text
+FROM
+  prompts;
+
+INSERT INTO
+  prompt_v2(
+    submission_id,
+    prompt_number,
+    prompt_date,
+    has_been_posted
+  )
+SELECT
+  s.submission_id,
+  p.prompt_number,
+  p.prompt_date,
+  E'1'
+FROM
+  prompts AS p
+JOIN
+  submission AS s
+ON
+  p.prompt_text = s.submission_text AND
+  p.suggesting_userid = s.suggesting_userid
+WHERE
+  p.has_been_posted = E'1';
