cf05eb6ef6d3ce4f12138fded111e6e3b6179f67
Alec Deitloff <alec.deitloff@gmail.com>
2019-01-13 12:07:53 -0800

    fix missing bold close tag in submission session wrapup

--- src/reactables/suggest-session/reactable.ts
+++ src/reactables/suggest-session/reactable.ts
@@ -9,78 +9,95 @@ import SuggestSessionReactableFactory from './factory';
 import SuggestSessionReactableShared from './shared';
 
 export default class SuggestSessionReactable extends ReactableType {
-    public readonly handle = SuggestSessionReactableShared.ReactableHandle;
+  public readonly handle = SuggestSessionReactableShared.ReactableHandle;
 
-    public async processReactionAdded(phil: Phil, post: ReactablePost, event: OfficialDiscordReactionEvent): Promise<any> {
-        const activeSession = await SubmissionSession.getActiveSession(phil, post.user.id);
-        if (!activeSession) {
-            return;
-        }
-
-        switch (event.emoji.name) {
-            case SuggestSessionReactableShared.Emoji.Stop: {
-                await this.stopSession(phil, post, activeSession);
-                break;
-            }
-
-            case SuggestSessionReactableShared.Emoji.MakeAnonymous: {
-                await this.makeSessionAnonymous(phil, post, activeSession);
-                break;
-            }
-        }
+  public async processReactionAdded(
+    phil: Phil,
+    post: ReactablePost,
+    event: OfficialDiscordReactionEvent
+  ): Promise<any> {
+    const activeSession = await SubmissionSession.getActiveSession(
+      phil,
+      post.user.id
+    );
+    if (!activeSession) {
+      return;
     }
 
-    private async stopSession(phil: Phil, post: ReactablePost, session: SubmissionSession) {
-        await post.remove(phil.db);
+    switch (event.emoji.name) {
+      case SuggestSessionReactableShared.Emoji.Stop: {
+        await this.stopSession(phil, post, activeSession);
+        break;
+      }
 
-        await session.end(phil);
-        await DiscordPromises.sendEmbedMessage(phil.bot, post.user.id, {
-            color: EmbedColor.Info,
-            description: this.getWrapupMessage(session),
-            title: ':ribbon: Suggestions Session Ended :ribbon:'
-        });
+      case SuggestSessionReactableShared.Emoji.MakeAnonymous: {
+        await this.makeSessionAnonymous(phil, post, activeSession);
+        break;
+      }
+    }
+  }
+
+  private async stopSession(
+    phil: Phil,
+    post: ReactablePost,
+    session: SubmissionSession
+  ) {
+    await post.remove(phil.db);
+
+    await session.end(phil);
+    await DiscordPromises.sendEmbedMessage(phil.bot, post.user.id, {
+      color: EmbedColor.Info,
+      description: this.getWrapupMessage(session),
+      title: ':ribbon: Suggestions Session Ended :ribbon:',
+    });
+  }
+
+  private getWrapupMessage(activeSession: SubmissionSession): string {
+    const NOWRAP = '';
+    let message = '';
+
+    const numSubmitted = activeSession.getNumberSubmissions();
+    if (numSubmitted > 0) {
+      const suggestion = numSubmitted === 1 ? 'suggestion' : 'suggestions';
+      const it = numSubmitted === 1 ? 'It' : 'They';
+      message += `Thank you for submitting your ${suggestion}! During this session, you ${NOWRAP} sent in **${numSubmitted}** ${suggestion}. ${it} will now ${NOWRAP} need to be processed by the server admins. Should ${it.toLowerCase()} ${NOWRAP} be approved, ${it.toLowerCase()} will go into the prompt queue and wind ${NOWRAP} up being posted one day! Thank you!!\n\n`;
     }
 
-    private getWrapupMessage(activeSession : SubmissionSession) : string {
-        const NOWRAP = '';
-        let message = '';
+    message += `Should you want to suggest something, just start a new session. I'm always ${NOWRAP} listening and eager to hear your suggestions!`;
 
-        const numSubmitted = activeSession.getNumberSubmissions();
-        if (numSubmitted > 0) {
-            const suggestion = (numSubmitted === 1 ? 'suggestion' : 'suggestions');
-            const it = (numSubmitted === 1 ? 'It' : 'They');
-            message += `Thank you for submitting your ${suggestion}! During this session, you ${
-                NOWRAP} sent in **${numSubmitted} ${suggestion}. ${it} will now ${
-                NOWRAP} need to be processed by the server admins. Should ${it.toLowerCase()} ${
-                NOWRAP} be approved, ${it.toLowerCase()} will go into the prompt queue and wind ${
-                NOWRAP} up being posted one day! Thank you!!\n\n`;
-        }
+    return message;
+  }
 
-        message += `Should you want to suggest something, just start a new session. I'm always ${
-            NOWRAP} listening and eager to hear your suggestions!`;
+  private async makeSessionAnonymous(
+    phil: Phil,
+    post: ReactablePost,
+    session: SubmissionSession
+  ) {
+    await post.remove(phil.db);
 
-        return message;
-    }
+    await session.makeAnonymous(phil);
+    const NOWRAP = '';
+    const messageId = await DiscordPromises.sendEmbedMessage(
+      phil.bot,
+      post.user.id,
+      {
+        color: EmbedColor.Info,
+        description: `All submissions you send during this session will be anonymous. Don't ${NOWRAP}worry though! You'll still get credit for them on the server leaderboard!`,
+        title: ':spy: Anonymous Session Begun :spy:',
+      }
+    );
 
-    private async makeSessionAnonymous(phil: Phil, post: ReactablePost, session: SubmissionSession) {
-        await post.remove(phil.db);
-
-        await session.makeAnonymous(phil);
-        const NOWRAP = '';
-        const messageId = await DiscordPromises.sendEmbedMessage(phil.bot, post.user.id, {
-            color: EmbedColor.Info,
-            description: `All submissions you send during this session will be anonymous. Don't ${
-                NOWRAP}worry though! You'll still get credit for them on the server leaderboard!`,
-            title: ':spy: Anonymous Session Begun :spy:'
-        });
-
-        const reactableFactory = new SuggestSessionReactableFactory(phil.bot, phil.db, {
-            canMakeAnonymous: false,
-            channelId: post.channelId,
-            messageId,
-            timeLimit: session.remainingTime.asMinutes(),
-            user: post.user
-        });
-        await reactableFactory.create();
-    }
+    const reactableFactory = new SuggestSessionReactableFactory(
+      phil.bot,
+      phil.db,
+      {
+        canMakeAnonymous: false,
+        channelId: post.channelId,
+        messageId,
+        timeLimit: session.remainingTime.asMinutes(),
+        user: post.user,
+      }
+    );
+    await reactableFactory.create();
+  }
 }
