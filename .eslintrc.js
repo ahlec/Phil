fe8f6b4c95089d6b74b9d74a9a674a99bc80be28
Alec Deitloff <alec.deitloff@gmail.com>
2020-03-14 21:21:55 -0700

    Initial changing from TSLint to ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'sort-keys': [
+      'error',
+      'asc',
+      {
+        natural: true,
+      },
+    ],
+  },
+};
